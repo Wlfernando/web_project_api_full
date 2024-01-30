e0d591a696e388380dfddce66dda7ea6407d279c
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-01-29 20:15:48 -0600

    feat: add logs

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 const notFound = require('./middleware/notFound');
 const hasError = require('./middleware/hasError');
+const { requestLogger, errorLogger } = require('./middleware/logger');
 const { validateMailAndPass } = require('./utils/utils');
 const authorize = require('./middleware/authorize');
 
@@ -26,6 +27,8 @@ app.use(express.json());
 
 app.use(cors({ origin: allowedOrigins }));
 
+app.use(requestLogger);
+
 app.use('/signup', validator, registerRouter);
 
 app.use('/signin', validator, loginRouter);
@@ -37,6 +40,8 @@ app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use('*', notFound);
+
+app.use(errorLogger);
 
 app.use(errors());
 
